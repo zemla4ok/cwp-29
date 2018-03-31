dc1386f2492ca8e18ae3c4f94b7d7bb5db41fd45
zemla4ok <dima-vital@live.com>
2018-03-31 08:46:33 +0300

    copied example from lection

---
+++ models/weapon.js
@@ -0,0 +1,13 @@
+module.exports = (Sequelize, sequelize) => {
+    return sequelize.define('weapons', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+  
+      name: Sequelize.STRING,
+      dps: Sequelize.INTEGER,
+    });
+  };
+  
